7300a06d4b9628a90a0af8241d7bdf2b57bbbe08
Leon Yao <leon.yao@shopee.com>
2023-09-20 17:42:34 +0800

    add dockerfile and scripts

---
+++ run_tgi_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CODE_DIR=$PWD
+DOCKERFILE=${CODE_DIR}/Dockerfile
+TAG=tgi_test_demo
+NAME=tgi_test_demo
+
+# build docker image
+#docker build --tag ${TAG} --file ${DOCKERFILE} .
+
+# run docker container
+docker run -d -it --rm --gpus all --ulimit memlock=-1 --ulimit stack=67108864  \
+    -v ${CODE_DIR}/models:/workspace/models \
+    -v ${CODE_DIR}:/workspace/ \
+    --name ${NAME} \
+    -p 5000:5000 \
+    starryskyyl/tgi_test_demo:latest
+
+# exec docker container
+docker exec -it ${NAME} /bin/bash
